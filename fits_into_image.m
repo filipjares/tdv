5076d6429f86fbe3ec46807497404abca77e62bd
Filip Jares <filipjares@post.cz>
2011-10-06 13:59:40 +0200

    fits_into_image(): use current axes dimensions
    instead of fixed image dimensions use use xlim and ylim of current axes

--- fits_into_image.m
+++ fits_into_image.m
@@ -1,10 +1,14 @@
 function indices = fits_into_image(A)
 
     Eps = eps*10^3;
-    X_LO = 1 - Eps;
-    X_HI = 800 + Eps;
-    Y_LO = 1 - Eps;
-    Y_HI = 600 + Eps;
+    
+    Xs = xlim;
+    Ys = ylim;
+    
+    X_LO = Xs(1) - Eps;
+    X_HI = Xs(2) + Eps;
+    Y_LO = Ys(1) - Eps;
+    Y_HI = Ys(2) + Eps;
 
     indices = A(1,:) >= X_LO & A(1,:) <= X_HI & A(2,:) >= Y_LO & A(2,:) <= Y_HI;
 

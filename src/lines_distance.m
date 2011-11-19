3475667be09ff77d858a45c4e96a27bb693f672c
Filip Jares <filipjares@post.cz>
2011-11-19 22:29:30 +0100

    add m-file to compute distance of two lines
    lines_distance.m gets two lines defined by point on them
    and tanget vector and returns
    
    - their distance and two vectors containing NaNs if given
      lines are parallel
    - their distance and vector defining points (on given lines)
      that define axis of given lines (if given lines intersect,
      returned vectors are the same and are equal to the point
      of intersection)

---
+++ src/lines_distance.m
@@ -0,0 +1,36 @@
+function [d, pA0, pB0] = lines_distance(pA, a, pB, b)
+    % [d, pA0, pB0] = find_lines_distance(pA, a, pB, b)
+    %
+    % Returns distance between two lines and returns points on each line
+    % that is the closest one to the other line
+    %
+    % Input:    pA  - point on line A
+    %           a   - tangent vector of the line A
+    %           pB  - point on line B
+    %           b   - tangent vector of the line B
+    % Output:   d   - distance between lines
+    %           pA0 - point on line A that is the closest one to the line B
+    %           pB0 - point on line B that is the closest one to the line A
+
+    pAB = pB - pA;
+    m = cross(b, a);
+    
+    if (norm(m) < 100*eps)
+        % lines are paralell
+        d = sqrt(pAB'*pAB - (pAB'*a/norm(a))^2);
+        pA0 = [NaN NaN NaN]';
+        pB0 = [NaN NaN NaN]';
+    else
+        % lines are skew or intersect
+        m2 = m'*m;
+        r = cross(pAB, m)/m2;
+
+        tA = r'*b;
+        tB = r'*a;
+
+        pA0 = pA + tA*a;
+        pB0 = pB + tB*b;
+        d = abs(pAB'*m)/sqrt(m2);
+    end
+
+end
--- src/perform_tests.m
+++ src/perform_tests.m
@@ -1,6 +1,7 @@
 
 %% Depth in camera function:
 
+clear all;
 P = [eye(3), zeros(3,1)];
 
 X = [0 0 1 1]';
@@ -21,3 +22,20 @@ end
 if ~err
     error('The depth_in_camera function should throw an error for infinite camera');
 end
+
+%% Lines distance function:
+
+clear all;
+
+v0 = [0 0 0]';
+EPS = 100*eps;
+
+[d, a, b] = lines_distance([-1 -1 0]', [1 1 0]', [1 -1 0]', [1 -1 0]');
+assert(d == 0 && all(a == v0) && all(b == v0));
+[d, a, b] = lines_distance([-1 -1 0]', [1 1 0]', [1 -1 1]', [1 -1 0]');
+assert(d == 1 && all(a == v0) && all(b == [0 0 1]'));
+[d, a, b] = lines_distance([-1 -1 0]', [1 1 0]', [1 -1 1]', [1 1 0]');
+assert(abs(d - sqrt(3)) < EPS && all(isnan(a)) && all(isnan(b)));
+
+
+

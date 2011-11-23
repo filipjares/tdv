798a29e1371b259a39d64b9ed46f754743f0af51
Filip Jares <filipjares@post.cz>
2011-11-23 10:42:23 +0100

    add two "empty" m-files for functions to be defined yet

---
+++ src/err_F_sampson.m
@@ -0,0 +1,8 @@
+function err = err_F_sampson( F, u1, u2 )
+    % err = err_F_sampson( F, u1, u2 )
+    %
+    % Sampson error on epipolar geometry
+    %
+    % 
+
+end
---
+++ src/u_correct_sampson.m
@@ -0,0 +1,12 @@
+function [nu1, nu2] = u_correct_sampson(F, u1, u2)
+    % [nu1, nu2] = u_correct_sampson(F, u1, u2)
+    %
+    % Sampson correction of correspondences
+    %
+    % Inputs:   F       - fundamental matrix (3×3)
+    %           u1, u2  - corresponding image points in homogeneous
+    %                     coordinates (3×n)
+    % Outputs:  nu1,nu2	- corrected corresponding points, homogeneous (3×n)
+
+
+end

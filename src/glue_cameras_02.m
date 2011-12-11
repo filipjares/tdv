7931caf32051debbedbb924a5355bf598a5176f4
Filip Jares <filipjares@post.cz>
2011-12-11 19:02:12 +0100

    Gluing cameras: prepare the loop (iterating through cameras left)

--- src/glue_cameras_02.m
+++ src/glue_cameras_02.m
@@ -42,63 +42,82 @@ corresp = corresp_start(corresp, i1, i2, find(best_inl_ix), 1:Xcount);
 
 % TODO
 
-%% Choosing the next camera to add
+%while true
+    %% Choosing the next camera to add
 
-% list of ('green') cameras with tentative scene-to-image correspondences
-ig = corresp_get_green_cameras(corresp);
-Xucount = corresp_get_Xucount(corresp, ig);
-[maxXucount, max_ig_ix] = max(Xucount);
-in = ig(max_ig_ix);
-fprintf('Camera number %u is the next\n', in);
-if (maxXucount < 3) % FIXME: what should be the limit?
-    fprintf('but it has less than 3 correspondences to the camera cloud\n');
-    return
-end
+    % list of ('green') cameras with tentative scene-to-image correspondences
+    ig = corresp_get_green_cameras(corresp);
+    if isempty(ig)
+        fprintf('There are no other cameras with tentative scene-to-image correspondences\n');
+%        break;
+        return;
+    end
+    Xucount = corresp_get_Xucount(corresp, ig);
+    [maxXucount, max_ig_ix] = max(Xucount);
+    in = ig(max_ig_ix);     % the new camera index
+    fprintf('Camera number %u is the next\n', in);
+    if (maxXucount < 3) % FIXME: what should be the limit?
+        fprintf('but it has less than 3 correspondences to the camera cloud\n');
+%        break;
+        return;
+    end
 
-% Scene-to-image correspondences in the new camera
-Xu = corresp_get_Xu(corresp, in);
+    % Scene-to-image correspondences in the new camera
+    Xu = corresp_get_Xu(corresp, in);
 
-%% Here comes the P3P RANSAC
+    %% Here comes the P3P RANSAC
 
-% TODO
+    % Scene-to-image correspondences (points)
+    Xn = X(:,Xu(:,1));
+    corresp_count = size(Xu,1);
+    un = zeros(2,corresp_count);
+    for i = 1:corresp_count
+        un(:,i) = [images(in).pts(Xu(i,2)).x; images(in).pts(Xu(i,2)).y];
+    end
+    un = e2p(un);
 
-% Scene-to-image correspondences (points)
-Xn = X(:,Xu(:,1));
-corresp_count = size(Xu,1);
-un = zeros(2,corresp_count);
-for i = 1:corresp_count
-    un(:,i) = [images(in).pts(Xu(i,2)).x; images(in).pts(Xu(i,2)).y];
-end
-un = e2p(un);
+    %% RANSAC
 
-%% RANSAC
+    [R, t, xinl] = p3p_ransac(K, Xn, un);
+    
+    corresp = corresp_join_camera(corresp, in, xinl);
+    cameras{in} = [R t];
 
-[R, t] = p3p_ransac(K, Xn, un);
+    %% Here comes another Bundle adjustment
+
+    % TODO
+    
+    %%
+    
+    ilist = corresp_get_cneighbours(corresp, in);
+    
+
+%end
 
 %% Vykreslit si obrazky
 
-u22 = zeros(2,size(Xu,1));
-X22 = corresp_get_Xu(corresp, 2);
-for i = 1:size(Xu(:,2))
-    u22(:,i) = [images(2).pts(X22(i,2)).x; images(2).pts(X22(i,2)).y];
-end
-
-figure(1);
-image(images(2).img);
-hold on;
-for i = 1:size(u22,2)
-    plot(u22(1,i), u22(2,i), 'ob', 'markerfacecolor', 'b');
-end
-hold off;
-axis equal
-figure(2)
-image(images(10).img);
-hold on;
-for i = 1:size(un,2)
-    plot(un(1,i), un(2,i), 'ob', 'markerfacecolor', 'b');
-end
-hold off;
-axis equal
+% u22 = zeros(2,size(Xu,1));
+% X22 = corresp_get_Xu(corresp, 2);
+% for i = 1:size(Xu(:,2))
+%     u22(:,i) = [images(2).pts(X22(i,2)).x; images(2).pts(X22(i,2)).y];
+% end
+% 
+% figure(1);
+% image(images(2).img);
+% hold on;
+% for i = 1:size(u22,2)
+%     plot(u22(1,i), u22(2,i), 'ob', 'markerfacecolor', 'b');
+% end
+% hold off;
+% axis equal
+% figure(2)
+% image(images(10).img);
+% hold on;
+% for i = 1:size(un,2)
+%     plot(un(1,i), un(2,i), 'ob', 'markerfacecolor', 'b');
+% end
+% hold off;
+% axis equal
 
 
 

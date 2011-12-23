4cf489af1ffc2ce940683b1741752d2e1625a0bb
Filip Jareš <filipjares@post.cz>
2011-12-23 18:51:53 +0100

    rectify & stereo - changes

--- src/rectify_and_stereo.m
+++ src/rectify_and_stereo.m
@@ -1,26 +1,25 @@
-function rectify_and_stereo(i1, i2)
-
-fprintf('Loading data from previous steps ...\n');
-load ('../data/glue_cameras-02-output.mat', 'K', 'cameras');
-images = initialize_empty_images_structure();
-
+%function rectify_and_stereo(i1, i2)
+i1 = 2;
+i2 = 6;
+addpath toolbox/
 addpath rectify/
 addpath gcs/
 
-% i1 = 2;
-% i2 = 6;
+fprintf('Loading data from previous steps ...\n');
+load ('../data/glue_cameras-02-output.mat', 'K', 'cameras', 'X');
+images = initialize_empty_images_structure();
 
 P1 = K*cameras{i1};
 P2 = K*cameras{i2};
 
 fprintf('Loading images %s) and %s).\n', images(i1).figId, images(i2).figId);
-im1=imread(images(i1).filename);
-im2=imread(images(i2).filename);
+images(i1).img=imread(images(i1).filename);
+images(i2).img=imread(images(i2).filename);
 
 F = cameras2F(P1,P2);
 
 fprintf('Computing rectifying homographies and rectifying images...\n');
-[H1, H2, im1r, im2r] = rectify(F, im1, im2);
+[H1, H2, im1r, im2r] = rectify(F, images(i1).img, images(i2).img);
 
 im1r = im1r(1:2:end, 1:2:end);
 im2r = im2r(1:2:end, 1:2:end);
@@ -36,7 +35,7 @@ P2r = H2*P2;
 %%
 
 pair_str = [num2str(i1, '%02u') '-' num2str(i2, '%02u')];
-% save(['../data/rectify_and_stereo_pair-', pair_str, '.mat']);
+save(['../data/rectify_and_stereo_pair-', pair_str, '.mat']);
 
 %%
 
@@ -54,22 +53,30 @@ for i = 1:size(im1r, 1)
 	n = n + pts_in_row;
 end
 
-X = Pu2X(P1r, P2r, u1, u2);
+%%
+tic;
+X2 = Pu2X(P1r, P2r, u1, u2);
+XX = X2(:,1:10:end);
+toc
+%%
+% save(['../data/points_from_stereo_pair_', pair_str, '.mat'], 'XX', 'P1r', 'P2r', 'H1', 'H2', 'P1', 'P2', 'F', 'i1', 'i2');
+% export_to_vrml(['../data/points_from_stereo_pair_', pair_str, '.wrl'], {P1, P2}, XX);
 
-% save(['../data/points_from_stereo_pair_', pair_str, '.mat'], 'X', 'P1r', 'P2r', 'H1', 'H2', 'P1', 'P2', 'F', 'i1', 'i2');
-% export_to_vrml(['../data/points_from_stereo_pair_', pair_str, '.wrl'], {P1, P2}, X);
+% export_to_vrml(['../data/points_from_stereo_pair_', pair_str, '-both_results.wrl'], {P1, P2}, [X, XX], [ [1 0 0]'*ones(1,size(X,2)), [0 1 0]'*ones(1,size(XX,2)) ]);
+
+save('../data/rectify_and_stereo-output.mat');
 
 %% Display rectified images
 
-figure(1); imshow(im1r);
-figure(2); imshow(im2r);
+% figure(1); imshow(im1r);
+% figure(2); imshow(im2r);
+% 
+% assert(size(im1r,1) == size(im2r,1));
+% umax = max(size(im1r,2), size(im2r,2));
+% 
+% j = 1000;
+% figure(1); h1 = line([1, umax], [j, j]);
+% figure(2); h2 = line([1, umax], [j, j]);
 
-assert(size(im1r,1) == size(im2r,1));
-umax = max(size(im1r,2), size(im2r,2));
 
-j = 1000;
-figure(1); h1 = line([1, umax], [j, j]);
-figure(2); h2 = line([1, umax], [j, j]);
-
-
-end
+%end

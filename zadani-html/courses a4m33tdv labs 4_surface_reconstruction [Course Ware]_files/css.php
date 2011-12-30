6a7a5d9b1e31bda774e64111424605262520e217
Filip Jareš <filipjares@post.cz>
2011-12-30 01:06:41 +0100

    add assignment for surface reconstruction ...

---
+++ zadani-html/courses a4m33tdv labs 4_surface_reconstruction [Course Ware]_files/css.php
@@ -0,0 +1,235 @@
+a.interwiki { background: transparent url(http://cw.felk.cvut.cz/lib/images/interwiki.png) 0px 1px no-repeat; padding-left: 16px;}a.iw_wp {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/wp.gif)}a.iw_wpfr {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/wpfr.gif)}a.iw_wpde {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/wpde.gif)}a.iw_wpes {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/wpes.gif)}a.iw_wppl {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/wppl.gif)}a.iw_wpjp {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/wpjp.gif)}a.iw_wpmeta {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/wpmeta.gif)}a.iw_doku {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/doku.gif)}a.iw_dokubug {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/dokubug.gif)}a.iw_amazon {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/amazon.gif)}a.iw_amazon_de {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/amazon.de.gif)}a.iw_amazon_uk {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/amazon.uk.gif)}a.iw_phpfn {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/phpfn.gif)}a.iw_coral {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/coral.gif)}a.iw_sb {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/sb.gif)}a.iw_google {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/google.gif)}a.iw_meatball {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/meatball.gif)}a.iw_wiki {  background-image: url(http://cw.felk.cvut.cz/lib/images/interwiki/wiki.gif)}a.mediafile { background: transparent url(http://cw.felk.cvut.cz/lib/images/fileicons/file.png) 0px 1px no-repeat; padding-left: 18px; padding-bottom: 1px;}a.mf_deb {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/deb.png)}a.mf_rar {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/rar.png)}a.mf_cpp {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/cpp.png)}a.mf_jpg {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/jpg.png)}a.mf_xls {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/xls.png)}a.mf_xml {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/xml.png)}a.mf_css {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/css.png)}a.mf_c {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/c.png)}a.mf_doc {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/doc.png)}a.mf_html {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/html.png)}a.mf_rpm {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/rpm.png)}a.mf_rtf {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/rtf.png)}a.mf_zip {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/zip.png)}a.mf_tar {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/tar.png)}a.mf_lua {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/lua.png)}a.mf_tgz {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/tgz.png)}a.mf_cs {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/cs.png)}a.mf_swf {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/swf.png)}a.mf_conf {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/conf.png)}a.mf_sxc {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/sxc.png)}a.mf_sxd {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/sxd.png)}a.mf_sxi {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/sxi.png)}a.mf_sxw {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/sxw.png)}a.mf_gz {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/gz.png)}a.mf_gif {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/gif.png)}a.mf_js {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/js.png)}a.mf_pl {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/pl.png)}a.mf_odc {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/odc.png)}a.mf_odf {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/odf.png)}a.mf_odg {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/odg.png)}a.mf_odi {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/odi.png)}a.mf_ps {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/ps.png)}a.mf_rb {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/rb.png)}a.mf_py {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/py.png)}a.mf_odp {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/odp.png)}a.mf_ods {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/ods.png)}a.mf_txt {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/txt.png)}a.mf_odt {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/odt.png)}a.mf_jpeg {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/jpeg.png)}a.mf_java {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/java.png)}a.mf_pdf {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/pdf.png)}a.mf_htm {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/htm.png)}a.mf_php {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/php.png)}a.mf_png {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/png.png)}a.mf_ppt {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/ppt.png)}a.mf_bz2 {  background-image: url(http://cw.felk.cvut.cz/lib/images/fileicons/bz2.png)}/********************************************************************
+Screen and Print Styles for the Wrap Plugin
+********************************************************************/
+
+/* tables in columns and boxes should span the whole width */
+.dokuwiki .wrap_column table,
+.dokuwiki .wrap_left table, .dokuwiki .wrap_right table, .dokuwiki .wrap_center table,
+.dokuwiki .wrap_box table,
+.dokuwiki .wrap_info table, .dokuwiki .wrap_important table, .dokuwiki .wrap_alert table, .dokuwiki .wrap_tip table, .dokuwiki .wrap_help table, .dokuwiki .wrap_todo table, .dokuwiki .wrap_download table {
+    width: 100%;
+}
+/* emulate a headline */
+.dokuwiki .wrap_column em strong,
+.dokuwiki .wrap_left em strong, .dokuwiki .wrap_right em strong, .dokuwiki .wrap_center em strong,
+.dokuwiki .wrap_box em strong,
+.dokuwiki .wrap_info em strong, .dokuwiki .wrap_important em strong, .dokuwiki .wrap_alert em strong, .dokuwiki .wrap_tip em strong, .dokuwiki .wrap_help em strong, .dokuwiki .wrap_todo em strong, .dokuwiki .wrap_download em strong {
+    font-size: 130%;
+    font-weight: bold;
+    font-style: normal;
+    display: block;
+}
+/* emulate a bigger headline with a bottom border */
+.dokuwiki .wrap_column em strong em.u,
+.dokuwiki .wrap_left em strong em.u, .dokuwiki .wrap_right em strong em.u, .dokuwiki .wrap_center em strong em.u,
+.dokuwiki .wrap_box em strong em.u,
+.dokuwiki .wrap_info em strong em.u, .dokuwiki .wrap_important em strong em.u, .dokuwiki .wrap_alert em strong em.u, .dokuwiki .wrap_tip em strong em.u, .dokuwiki .wrap_help em strong em.u, .dokuwiki .wrap_todo em strong em.u, .dokuwiki .wrap_download em strong em.u {
+    font-size: 115%;
+    border-bottom: 1px solid #4d5f67;
+    font-style: normal;
+    text-decoration: none;
+    display: block;
+}
+/* different bigger headline for safety notes */
+.dokuwiki .wrap_danger em strong em.u, .dokuwiki .wrap_warning em strong em.u, .dokuwiki .wrap_caution em strong em.u, .dokuwiki .wrap_notice em strong em.u, .dokuwiki .wrap_safety em strong em.u {
+    font-size: 115%;
+    font-style: normal;
+    text-decoration: none;
+    display: block;
+    text-transform: uppercase;
+    border-bottom-width: 0;
+}
+/* change border colour of emulated headlines inside boxes to something more neutral
+   (to match all the different background colours) */
+.dokuwiki .wrap_box em strong em.u,
+.dokuwiki .wrap_info em strong em.u, .dokuwiki .wrap_important em strong em.u, .dokuwiki .wrap_alert em strong em.u, .dokuwiki .wrap_tip em strong em.u, .dokuwiki .wrap_help em strong em.u, .dokuwiki .wrap_todo em strong em.u, .dokuwiki .wrap_download em strong em.u {
+    border-bottom-color: #999;
+}
+
+
+/* columns
+********************************************************************/
+
+.dokuwiki .wrap_left,
+.dokuwiki .wrap_column {
+    float: left;
+    margin-right: 1.5em;
+}
+.dokuwiki .wrap_right {
+    float: right;
+    margin-left: 1.5em;
+}
+.dokuwiki .wrap_center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+/*
+.dokuwiki .wrap_column,
+.dokuwiki .wrap_left,
+.dokuwiki .wrap_right {
+    overflow: hidden;
+}
+*/
+.dokuwiki .wrap_container {
+    margin-right: -1.5em;
+}
+
+
+/* alignments
+********************************************************************/
+
+.dokuwiki .wrap_leftalign {
+    text-align: left;
+}
+.dokuwiki .wrap_centeralign {
+    text-align: center;
+}
+.dokuwiki .wrap_rightalign {
+    text-align: right;
+}
+.dokuwiki .wrap_justify {
+    text-align: justify;
+}
+
+
+/* box
+********************************************************************/
+
+/* see styles for boxes and notes with icons in style.css */
+
+/*____________ rounded corners ____________*/
+/* (only for modern browsers) */
+
+.dokuwiki div.wrap_round {
+    border-radius: 20px;
+    -moz-border-radius: 20px;
+    -webkit-border-radius: 20px;
+    -khtml-border-radius: 20px;
+}
+.dokuwiki span.wrap_round {
+    border-radius: 2px;
+    -moz-border-radius: 2px;
+    -webkit-border-radius: 2px;
+    -khtml-border-radius: 2px;
+}
+
+
+/* mark
+********************************************************************/
+
+div.dokuwiki .wrap_lo {
+    color: #666;
+    font-size: 85%;
+}
+div.dokuwiki .wrap_em {
+    color: #c00;
+    font-weight: bold;
+}
+
+/* see styles for highlighted text in style.css */
+
+
+/* typography
+********************************************************************/
+
+/*____________ fonts ____________*/
+
+div.dokuwiki .wrap_sansserif {
+    font-family: Verdana,Tahoma,Geneva,"DejaVu Sans","Bitstream Vera Sans","Liberation Sans", Arial,Helvetica,FreeSans,"Liberation Sans","Nimbus Sans L", sans-serif;
+}
+div.dokuwiki .wrap_serif {
+    font-family: Georgia,Garamond,"Palatino Linotype","Book Antiqua",Palatino,Palladio,"URW Palladio L","Liberation Serif",Didot,Gentium,"Bitstream Charter","Century Schoolbook L", "Times New Roman",Times,"Nimbus Roman No9 L","FreeSerif", serif;
+}
+div.dokuwiki .wrap_monospace {
+    font-family: Courier,"Courier New",FreeMono,"Nimbus Mono L","Liberation Mono", "Lucida Console",Monaco,"DejaVu Sans Mono","Bitstream Vera Sans Mono", monospace;
+}
+
+/*____________ font size ____________*/
+
+div.dokuwiki .wrap_bigger {
+    font-size: 125%;
+}
+div.dokuwiki .wrap_muchbigger {
+    font-size: 200%;
+}
+div.dokuwiki .wrap_smaller {
+    font-size: 75%;
+}
+
+/*____________ font colours ____________*/
+
+div.dokuwiki .wrap_fgred {
+    color: #900;
+}
+div.dokuwiki .wrap_fggreen {
+    color: #090;
+}
+div.dokuwiki .wrap_fgblue {
+    color: #009;
+}
+div.dokuwiki .wrap_fgcyan {
+    color: #099;
+}
+div.dokuwiki .wrap_fgviolet {
+    color: #909;
+}
+div.dokuwiki .wrap_fgyellow {
+    color: #990;
+}
+div.dokuwiki .wrap_fggrey {
+    color: #666;
+}
+div.dokuwiki .wrap_fgblack {
+    color: #000;
+}
+
+/* see styles for background colours and white font colour in style.css */
+
+
+/* miscellaneous
+********************************************************************/
+
+/*____________ indent ____________*/
+
+div.dokuwiki .wrap_indent {
+    padding-left: 1.5em;
+}
+
+/*____________ outdent ____________*/
+
+div.dokuwiki .wrap_outdent {
+    margin-left: -1.5em;
+}
+
+/*____________ word wrapping in pre ____________*/
+
+div.dokuwiki div.wrap_prewrap pre {
+    white-space: pre-wrap;
+    word-wrap: break-word;/* for IE < 8 */
+    /* white-space: -moz-pre-wrap; for FF < 3 */
+}
+
+/*____________ spoiler ____________*/
+
+div.dokuwiki div.wrap_spoiler {
+    margin-bottom: 1.5em;
+}
+/* see rest of spoiler styles in style.css */
+
+/*____________ clear float ____________*/
+
+div.dokuwiki .wrap_clear {
+    clear: both;
+    line-height: 0;
+    height: 0;
+    font-size: 1px;
+    visibility: hidden;
+    overflow: hidden;
+}
+
+/*____________ hide ____________*/
+
+div.dokuwiki .wrap_hide {
+    display: none;
+}
